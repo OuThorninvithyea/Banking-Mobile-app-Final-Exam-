549b5c773dbf1565df9f9b1369c6fc2d8b7d9e60
Shawnnnnhh <100029619+Shawnnnnhh@users.noreply.github.com>
2026-03-07 11:44:55 +0700

    feat: Add `SerializedName` annotation to `accountNumber` in `User` data class for JSON mapping.

--- app/src/main/java/com/hustle/bankapp/data/User.kt
+++ app/src/main/java/com/hustle/bankapp/data/User.kt
@@ -1,9 +1,10 @@
 package com.hustle.bankapp.data
 
+import com.google.gson.annotations.SerializedName
 data class User(
     val id: String,
     val name: String,
-    val accountNumber: String,
+    @SerializedName("account_number") val accountNumber: String,
     val balance: Double,
     val email: String = "",
     val contact: String = ""
